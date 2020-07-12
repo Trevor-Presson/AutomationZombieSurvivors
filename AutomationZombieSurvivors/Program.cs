8e56216ef46453aad668a0134942bbb5b92ff2cc
Trevor Presson <trevorpresson@gmail.com>
2020-07-12 18:37:16 -0500

    Add project files.

---
+++ AutomationZombieSurvivors/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AutomationZombieSurvivors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
